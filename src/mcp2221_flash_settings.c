541793c543617c40e88ae7b23b26a5c4a07aaf99
Tobias X. Vogel <tobias.vogel@linux.com>
2026-07-31 21:48:19 +0200

    use canonical types in flash settings API

--- src/mcp2221_flash_settings.c
+++ src/mcp2221_flash_settings.c
@@ -4,7 +4,7 @@
 #include "mcp2221_flash.h"
 #include "exceptions.h"
 
-int mcp2221_flash_get_settings(mcp2221_t *dev, MCP2221_FlashSettings *st) {
+int mcp2221_flash_get_settings(mcp2221_t *dev, mcp2221_flash_settings_t *st) {
 	int err;
 
 	err = mcp2221_flash_read(dev, MCP2221_FLASH_DATA_CHIP_SETTINGS, st->chip_settings);
