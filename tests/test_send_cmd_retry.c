18fdb7e64f5adc05ff83bc6cfac4b9fd26224717
Tobias X. Vogel <tobias.vogel@linux.com>
2026-08-13 19:21:40 +0200

    test: cover I2C command failure mapping

--- tests/test_send_cmd_retry.c
+++ tests/test_send_cmd_retry.c
@@ -5,15 +5,19 @@
 #include <libusb.h>
 
 #include "mcp2221_constants.h"
+#include "mcp2221_internal_constants.h"
 
 static int mock_write_count;
 static int mock_read_count;
 static int mock_mode;
+static uint8_t mock_last_cmd;
 
 enum {
 	MOCK_READ_TIMEOUT = 1,
 	MOCK_TIMEOUT_THEN_OK,
-	MOCK_PROTOCOL_ERROR
+	MOCK_PROTOCOL_ERROR,
+	MOCK_I2C_GET_DATA_NACK,
+	MOCK_I2C_GET_DATA_UNKNOWN_ERROR
 };
 
 int libusb_interrupt_transfer(libusb_device_handle *dev_handle, unsigned char endpoint,
@@ -26,6 +30,7 @@ int libusb_interrupt_transfer(libusb_device_handle *dev_handle, unsigned char en
 
 	if ((endpoint & LIBUSB_ENDPOINT_DIR_MASK) == LIBUSB_ENDPOINT_OUT) {
 		mock_write_count++;
+		mock_last_cmd = data[0];
 		*transferred = length;
 		return 0;
 	}
@@ -43,6 +48,24 @@ int libusb_interrupt_transfer(libusb_device_handle *dev_handle, unsigned char en
 	}
 
 	memset(data, 0, (size_t)length);
+
+	if (mock_mode == MOCK_I2C_GET_DATA_NACK ||
+	    mock_mode == MOCK_I2C_GET_DATA_UNKNOWN_ERROR) {
+		data[MCP2221_RESPONSE_ECHO_BYTE] = mock_last_cmd;
+		data[MCP2221_RESPONSE_STATUS_BYTE] = MCP2221_RESPONSE_RESULT_OK;
+
+		if (mock_last_cmd == MCP2221_CMD_I2C_READ_DATA_GET_I2C_DATA) {
+			data[MCP2221_RESPONSE_STATUS_BYTE] = 0x41;
+			data[MCP2221_I2C_INTERNAL_STATUS_BYTE] =
+				(mock_mode == MOCK_I2C_GET_DATA_NACK)
+					? MCP2221_I2C_ST_WRADDRL_NACK_STOP
+					: 0xFF;
+		}
+
+		*transferred = length;
+		return 0;
+	}
+
 	data[MCP2221_RESPONSE_ECHO_BYTE] =
 		(mock_mode == MOCK_PROTOCOL_ERROR)
 			? MCP2221_CMD_GET_GPIO_VALUES
@@ -63,6 +86,7 @@ static void reset_mock(int mode) {
 	mock_write_count = 0;
 	mock_read_count = 0;
 	mock_mode = mode;
+	mock_last_cmd = 0;
 }
 
 static mcp2221_t make_test_device(void) {
@@ -113,9 +137,42 @@ static void test_retry_safe_does_not_retry_protocol_error(void) {
 	assert(mock_read_count == 1);
 }
 
+
+static void test_i2c_command_failure_maps_nack(void) {
+	mcp2221_t dev = make_test_device();
+	uint8_t data;
+
+	reset_mock(MOCK_I2C_GET_DATA_NACK);
+
+	assert(mcp2221_i2c_read_ex(
+		&dev,
+		0x50,
+		&data,
+		1,
+		MCP2221_I2C_KIND_NORMAL,
+		100) == MCP2221_ERR_NOT_ACK);
+}
+
+static void test_i2c_command_failure_maps_unknown_state(void) {
+	mcp2221_t dev = make_test_device();
+	uint8_t data;
+
+	reset_mock(MOCK_I2C_GET_DATA_UNKNOWN_ERROR);
+
+	assert(mcp2221_i2c_read_ex(
+		&dev,
+		0x50,
+		&data,
+		1,
+		MCP2221_I2C_KIND_NORMAL,
+		100) == MCP2221_ERR_I2C);
+}
+
 int main(void) {
 	test_public_send_is_single_shot();
 	test_retry_safe_retries_timeout();
 	test_retry_safe_does_not_retry_protocol_error();
+	test_i2c_command_failure_maps_nack();
+	test_i2c_command_failure_maps_unknown_state();
 	return 0;
 }
